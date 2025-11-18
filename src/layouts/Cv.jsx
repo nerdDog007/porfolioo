a41a46eff43ce54a976fb7f3b973fc516dc6b93e
sandeshkc <sandeshkhatri007@gmail.com>
2025-11-18 17:48:23 +0545

    D

--- src/layouts/Cv.jsx
+++ src/layouts/Cv.jsx
@@ -6,8 +6,8 @@ function Cv() {
   return (
     <div className='flex flex-col items-center h-fit  justify-center'>
         <h1 className='text-4xl mt-4 p-4 font-semibold'>My CV</h1>
-        <img src={two} alt="" className='w-screen h-fit object-fill md:w-[60%] lg:w-[40%]'/>
         <img src={one} alt="" className='w-screen h-fit object-fill md:w-[60%] lg:w-[40%]'/>
+        <img src={two} alt="" className='w-screen h-fit object-fill md:w-[60%] lg:w-[40%]'/>
     </div>
   )
 }
